e7fe47526ab522cb67b3e603bb30190f0c033346
Anton Sozonenko <sozonenko.anton@gmail.com>
2023-02-22 20:20:13 +0200

    finish 1-st task

---
+++ homework_6/task_1.py
@@ -0,0 +1,17 @@
+def arifmetic_func(num_1: int, num_2: int, operator):
+    if operator == '+':
+        result = num_1 + num_2
+    elif operator == '-':
+        result = num_1 - num_2
+    elif operator == '/':
+        result = num_1 / num_2
+    elif operator == '*':
+        result = num_1 * num_2
+    else:
+        print('Not known operation:', operator)
+    return result
+
+
+if __name__ == '__main__':
+    result = arifmetic_func(10, 3, '*')
+    print(result)
