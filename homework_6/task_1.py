3684e6514cc55b0bb80316ce0af4127449a30bfd
Anton Sozonenko <sozonenko.anton@gmail.com>
2023-02-23 15:07:34 +0200

    finish 2-d task

--- homework_6/task_1.py
+++ homework_6/task_1.py
@@ -9,9 +9,10 @@ def arifmetic_func(num_1: int, num_2: int, operator):
         result = num_1 * num_2
     else:
         print('Not known operation:', operator)
+        return None
     return result
 
 
 if __name__ == '__main__':
-    result = arifmetic_func(10, 3, '*')
+    result = arifmetic_func(10, 3, '1')
     print(result)
