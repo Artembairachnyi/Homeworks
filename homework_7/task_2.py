ed586ea8b6266bd88c0c8f18dcede63195d34df4
Anton Sozonenko <sozonenko.anton@gmail.com>
2023-03-03 17:12:57 +0200

    update 1-st task

--- homework_7/task_2.py
+++ homework_7/task_2.py
@@ -1,15 +1,15 @@
 users_id = [75, 83, 82, 14, 21, 22, 51, 12, 28]
 
 
-def sort_func(variable: int):
-
-    if variable % 2 == 0:
+def sort_func(number):
+    if number % 2 == 0:
         return True
 
-    return False
+
+def filter_func(func, sequense):
+    return [i for i in sequense if func(i)]
 
 
 if __name__ == '__main__':
-    filtered = filter(sort_func, users_id)
-    for a in filtered:
-        print(a)
+    filtered_id = filter_func(sort_func, users_id)
+    print(filtered_id)
